66e5dc37adf95eb6b006e058c5419979b3315915
Romeyn Gibson <romeyngibson@Romeyns-MacBook-Pro.local>
2019-09-06 17:09:19 -0400

    Continuign work

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    console.log("ready");
+
+    $("#login-form").fadeIn({queue:false,duration: 'slow'});
+    $("#login-form").animate({top: '250px'},'slow');
+    
+
+});
